690984816b655869c4d57701892639ad9273929f
Charlie Pu <charliepu123@163.com>
2020-04-26 13:13:19 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace My_Date_Countdown
+{
+    public sealed partial class MainPage : Page
+    {
+        private DateTimeOffset targetDate;
+        private String title;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void ButtonSet_Click(object sender, RoutedEventArgs e)
+        {
+            targetDate = DatePickerTargetDate.Date;
+            title = TextBlockTitle.Text;
+
+            TextBlockTitle.Text = title;
+            int DateDifference = (int)(targetDate - DateTime.Today).TotalDays;
+            TextBlockDays.Text = DateDifference.ToString() + " days left";
+        }
+    }
+}
---
+++ MyStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    class MyStorage
+    {
+        private readonly Windows.Storage.ApplicationDataContainer localSettings;
+
+        public MyStorage()
+        {
+            localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+        }
+
+        public void Store(string key, object value)
+        {
+            localSettings.Values[key] = value;
+        }
+
+        public object Get(string key)
+        {
+            return localSettings.Values[key];
+        }
+    }
+}
