69af0559111f729be35587281c53687ab715c5f2
Charlie Pu <charliepu123@163.com>
2020-04-26 14:02:12 +0800

    Apply storage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -19,22 +19,56 @@ namespace My_Date_Countdown
     public sealed partial class MainPage : Page
     {
         private DateTimeOffset targetDate;
-        private String title;
+        private string title;
+        private MyStorage storage;
 
         public MainPage()
         {
             this.InitializeComponent();
+            storage = new MyStorage();
+            LoadDataFromStorage();
+            SetDisplay();
+        }
 
+        private void SetDisplay()
+        {
+            TextBlockTitle.Text = title;
+            int DateDifference = (int)(targetDate - DateTime.Today).TotalDays;
+            TextBlockDays.Text = DateDifference.ToString() + " days left";
+        }
+
+        private void LoadDataFromStorage()
+        {
+            if (storage.Get("title") != null)
+            {
+                title = (string)storage.Get("title");
+            }
+            else
+            {
+                title = "";
+            }
+            if (storage.Get("targetDate") != null)
+            {
+                targetDate = (DateTimeOffset)storage.Get("targetDate");
+            }
+            else
+            {
+                targetDate = DateTimeOffset.Now;
+            }
+        }
+
+        private void StoreData()
+        {
+            storage.Store("title", title);
+            storage.Store("targetDate", targetDate);
         }
 
         private void ButtonSet_Click(object sender, RoutedEventArgs e)
         {
             targetDate = DatePickerTargetDate.Date;
-            title = TextBlockTitle.Text;
-
-            TextBlockTitle.Text = title;
-            int DateDifference = (int)(targetDate - DateTime.Today).TotalDays;
-            TextBlockDays.Text = DateDifference.ToString() + " days left";
+            title = TextBoxTitle.Text;
+            SetDisplay();
+            StoreData();
         }
     }
 }
