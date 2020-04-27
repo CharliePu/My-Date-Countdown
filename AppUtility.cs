a1967fa29683be6027cb21c7a74f67dcaedd0171
Charlie Pu <charliepu123@163.com>
2020-04-27 10:44:29 +0800

    Apply grid layout, add acrylic effect, and change app icon

---
+++ AppUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+
+namespace My_Date_Countdown
+{
+    class AppUtility
+    {
+        public AppUtility setMinWindowSize(double width, double height)
+        {
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(
+                new Size(
+                    450,
+                    500
+                    )
+                );
+
+            return this;
+        }
+
+        public AppUtility setTitleBarTranslucent(bool state)
+        {
+            ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = state;
+
+            return this;
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,8 @@ namespace My_Date_Countdown
         public MainPage()
         {
             this.InitializeComponent();
+
+            new AppUtility().setMinWindowSize(450, 500).setTitleBarTranslucent(true);
             storage = new MyStorage();
             LoadDataFromStorage();
             SetDisplay();
