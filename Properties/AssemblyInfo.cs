e09db2a326cf75155edb0537fcc075b8670ca4bf
Charlie Pu <charliepu123@163.com>
2020-04-28 10:06:08 +0800

    Rename project

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("My Date Countdown")]
+[assembly: AssemblyTitle("My_Date_Countdown")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("My Date Countdown")]
+[assembly: AssemblyProduct("My_Date_Countdown")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
