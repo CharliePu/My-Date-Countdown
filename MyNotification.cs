944b5e09f9f5b466c5ac40ffc3fcd4e690889ef0
CharliePu <charliepu123@163.com>
2021-06-20 20:40:20 +0800

    Add separate controls for tile and toast

--- MyNotification.cs
+++ MyNotification.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Threading.Tasks;
 using Windows.UI.Notifications;
 
 
@@ -7,17 +9,14 @@ namespace My_Date_Countdown
     // Make sure to install Microsoft.Toolkit.Uwp.Notifications on NuGet
     class MyNotification
     {
-        string title, content;
         const string tag = "tag";
 
 
-        public MyNotification(string title, string content = "")
+        public MyNotification()
         {
-            this.title = title;
-            this.content = content;
         }
 
-        public MyNotification ToastNotify(string tag = tag)
+        public MyNotification ToastNotify(string title = "", string content = "", string tag = tag)
         {
             ToastVisual visual = new ToastVisual()
             {
@@ -51,8 +50,49 @@ namespace My_Date_Countdown
             return this;
         }
 
-        public MyNotification TileNotify()
+        public void ClearTile()
         {
+            TileUpdateManager.CreateTileUpdaterForApplication().Clear();
+        }
+        private async Task<bool> CheckTileSupport()
+        {
+            // Check for pin-to-start support
+            // https://docs.microsoft.com/en-us/windows/uwp/design/shell/tiles-and-notifications/primary-tile-apis
+            Windows.ApplicationModel.Core.AppListEntry entry = (await Windows.ApplicationModel.Package.Current.GetAppListEntriesAsync())[0];
+            bool isSupported = Windows.UI.StartScreen.StartScreenManager.GetDefault().SupportsAppListEntry(entry);
+            return isSupported;
+        }
+
+        public async Task<bool> CheckTileExist()
+        {
+            //https://docs.microsoft.com/en-us/windows/uwp/design/shell/tiles-and-notifications/primary-tile-apis#check-whether-youre-currently-pinned
+            // Get your own app list entry
+            Windows.ApplicationModel.Core.AppListEntry entry = (await Windows.ApplicationModel.Package.Current.GetAppListEntriesAsync())[0];
+
+            // Check if your app is currently pinned
+            bool isPinned = await Windows.UI.StartScreen.StartScreenManager.GetDefault().ContainsAppListEntryAsync(entry);
+
+            return isPinned;
+        }
+
+        public async void PinTile()
+        {
+            // https://docs.microsoft.com/en-us/windows/uwp/design/shell/tiles-and-notifications/primary-tile-apis#pin-your-primary-tile
+            // Get your own app list entry
+            Windows.ApplicationModel.Core.AppListEntry entry = (await Windows.ApplicationModel.Package.Current.GetAppListEntriesAsync())[0];
+
+            // And pin it to Start
+            await Windows.UI.StartScreen.StartScreenManager.GetDefault().RequestAddAppListEntryAsync(entry);
+        }
+
+        public async Task<bool> CheckToastPermission()
+        {
+            return await new AppUtility().requestStartupTaskAsync("DateCountdownStartupId");
+        }
+
+        public MyNotification TileNotify(string title = "", string content = "")
+        {
+
             // Construct the tile content
             TileContent tileContent = new TileContent()
             {
